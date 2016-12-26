b3b3c1f7f050d5a282555ab3419c25ded2114fef
public <946217824@qq.com>
2016-12-26 15:31:00 +0800

    修改server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 var path=require('path');
 var express=require('express');
 var proxy=require('http-proxy-middleware');
+var nodegrass=require('nodegrass');
+var sha1=require('sha1');
 //引入转发请求插件
 //var proxy=require('http-proxy-middleware');
 //实例express
@@ -15,6 +17,28 @@ app.use('/',express.static(viewsPath));
 var publicPath=path.join(__dirname,'public');
 app.use('/public',express.static(publicPath));
 
+
+//定义验证接口
+app.use('/weixin', function(req, res){
+	//获取get传递数据
+	var obj = req.query;
+	console.log("weixin", obj);
+	//将数据添加到一个数组
+	var arr = ["nodemodule", obj.timestamp, obj.nonce];
+	//排序
+	arr.sort();
+	//拼接字符串，并进行 sha1 加密
+	var str = sha1(arr.join(""));
+	console.log('sha1   ', str);
+	console.log('signature', obj.signature === str);
+	//匹配是否是微信请求
+	if( obj.signature === str){
+		//成功返回 echostr 随机字符串
+		res.send(obj.echostr).end();
+	}else{
+		res.send("验证失败").end();
+	}
+});
 //定义一个接口，端口号输入127.0.0.1:9999/login
 app.get('/login',function(req,res){
 	res.send('ha ha ha');
